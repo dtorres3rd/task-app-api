ca1bcb509c2302f74ea2a90b3c8da019e94c9204
Daniel Torres III <daniel.torres3rd@gmail.com>
2024-11-25 21:33:01 +0800

    refactored code for resource - user

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 const express = require('express');
+//ensure mongoose database will run upon loading
+require('./db/mongoose')
+const User = require('./models/user')
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+//automatically parse incoming JSON to object, so it can be accessed in request handler
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then(() => {
+      res.send(user);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
