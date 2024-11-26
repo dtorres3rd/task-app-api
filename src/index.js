57b30239ed4ad1ed5d70d6ebf9a6c2193f92ce55
Daniel Torres III <daniel.torres3rd@gmail.com>
2024-11-26 10:47:51 +0800

    added get endpoint for resource tasks

--- src/index.js
+++ src/index.js
@@ -24,6 +24,29 @@ app.post('/users', (req, res) => {
     });
 });
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) =>{
+      res.send(users)
+    }).catch((e) => {
+      res.status(500).send(e);
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+  User.find({}).then((users) =>{
+    const _id = req.params.id
+
+    User.findById(_id).then((user) =>{
+      if (!user){
+        return res.status(404).send()
+      }
+      res.send(user)
+    })
+  }).catch((e) => {
+    res.status(500).send(e);
+  })
+})
+
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body);
 
@@ -37,6 +60,28 @@ app.post('/tasks', (req, res) => {
     });
 });
 
+app.get('/tasks', (req, res) => {
+  Task.find({}).then((tasks) =>{
+    res.send(tasks)
+  }).catch((e) => {
+    res.status(500).send(e);
+  })
+})
+
+app.get('/tasks/:id', (req, res) => {
+  Task.find({}).then((tasks) =>{
+    const _id = req.params.id
+
+    Task.findById(_id).then((task) =>{
+      if (!task){
+        return res.status(404).send()
+      }
+      res.send(task)
+    })
+  }).catch((e) => {
+    res.status(500).send(e);
+  })
+})
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
