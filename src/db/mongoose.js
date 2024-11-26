954c3d7abcfc70dda1815ed8343e91af8de63cb5
Daniel Torres III <daniel.torres3rd@gmail.com>
2024-11-26 09:38:25 +0800

    added create endpoint for tasks

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,15 +4,3 @@ const keys = require('../../config/keys');
 const mongoose = require('mongoose');
 
 mongoose.connect(keys.mongoURI);
-
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-});
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 //ensure mongoose database will run upon loading
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
+
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,12 +17,26 @@ app.post('/users', (req, res) => {
   user
     .save()
     .then(() => {
-      res.send(user);
+      res.status(201).send(user);
     })
     .catch((e) => {
       res.status(400).send(e);
     });
 });
+
+app.post('/tasks', (req, res) => {
+  const task = new Task(req.body);
+
+  task
+    .save()
+    .then(() => {
+      res.status(201).send(task);
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
