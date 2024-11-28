e828279cfff5951d4bce8976e530d83e37fa46d8
Daniel Torres III <daniel.torres3rd@gmail.com>
2024-11-28 11:27:18 +0800

    added inital endpoint for user login

--- src/models/user.js
+++ src/models/user.js
@@ -20,6 +20,7 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique: true,
     trim: true,
     lowercase: true,
     validate(value) {
@@ -42,8 +43,26 @@ const userSchema = new mongoose.Schema({
 /*
 - setup middleware for USER mongoose schema.
 - execute this callback async function before saving.
-- this insude the callback function is equivalent to the document being saved
+- the this pointer inside the callback function is equivalent to the document being saved
 */
+
+// this is a user creatd function "findByCredentials"
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    throw new Error('Unable to login1') 
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password)
+
+  if (!isMatch){
+    throw new Error('Unable to login2') 
+  }
+
+  return user
+}
+
+// hash the plain text password before saving
 userSchema.pre('save', async function (next) {
   const user = this
 
