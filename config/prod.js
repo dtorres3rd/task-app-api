d2acd705805cd3f3bff28397350167c26e1d9680
Daniel Torres III <daniel.torres3rd@gmail.com>
2024-12-04 10:53:29 +0800

    fixed env var refence

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,9 @@
 module.exports= {
     PORT: process.env.PORT,
+    MAINTENANCE_MODE: MAINTENANCE_MODE,
     JWT_SECRET: process.env.JWT_SECRET,
     mongoURI: process.env.MONGO_URI,
-    mailgunAPIKey: process.env.MAILGUN_DOMAIN,
+    mailgunAPIKey: process.env.MAILGUN_API_KEY,
+    mailgunDomain: process.env.MAILGUN_DOMAIN,
     mailgunFromEmail: process.env.MAILGUN_FROM_EMAIL,
 };
